ce7ed00ecb977318b3385ff40582b12353a0f13e
JoyontoJoyXD <135032984+JoyontoJoyXD@users.noreply.github.com>
2023-09-23 16:57:02 +0600

    initial commit

---
+++ Django-Startup-Project/Django-advance-project-STI-Bangalore/startupproject/authapp/views.py
@@ -0,0 +1,124 @@
+from django.shortcuts import render,redirect
+from django.contrib import messages
+import re
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.views.generic import View 
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_decode,urlsafe_base64_encode
+from authapp.utils import TokenGenerator,generate_token
+from django.utils.encoding import force_bytes,force_text,DjangoUnicodeDecodeError
+from django.core.mail import EmailMessage
+from django.conf import settings
+
+# Create your views here.
+
+
+def signup(request):
+    flag=0
+    if request.method=="POST":
+        name=request.POST['name']
+        email=request.POST['email']
+        password=request.POST['pass1']
+        confirm_password=request.POST['pass2']
+        # print(name,email,password,confirm_password)
+        if password!=confirm_password:
+            messages.warning(request,"Password is Not Matching")
+            return redirect('/auth/signup/') 
+        if len(password)<=5:
+            messages.warning(request,"Password must be atleast 5 character")
+            return redirect('/auth/signup/') 
+        elif not re.search("[a-z]", password):
+            flag = -1
+            
+        elif not re.search("[A-Z]", password):
+            flag = -1
+            
+        elif not re.search("[0-9]", password):
+            flag = -1
+            
+        elif not re.search("[_@$#%^*()-]" , password):
+            flag = -1  
+        else:
+            pass
+        if(flag==0):
+            try:
+                if User.objects.get(username=email):
+                # return HttpResponse("email already exist")
+                    messages.info(request,"Email is Taken")
+                    return redirect('/auth/signup/') 
+
+
+            except Exception as identifier:
+                pass
+            user = User.objects.create_user(email,email,password)
+            user.first_name=name
+            user.is_active=False
+            user.save()
+            email_subject="Activate Your Account"
+            message=render_to_string('activate.html',{
+                'user':user,
+                'domain':'127.0.0.1:8000',
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':generate_token.make_token(user)
+
+            })
+
+            # email_message = EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email])
+            # email_message.send()
+            messages.success(request,f"Activate Your Account by clicking the link in your gmail {message}")
+            # messages.success(request,f"{message}")
+            return redirect('/auth/login/') 
+
+        else:
+            messages.error(request,"Password is not valid")
+            return redirect('/auth/signup/') 
+        
+
+
+    return render(request,"signup.html")
+
+
+
+
+class ActivateAccountView(View):
+    def get(self,request,uidb64,token):
+        try:
+            uid=force_text(urlsafe_base64_decode(uidb64))
+            user=User.objects.get(pk=uid)
+        except Exception as identifier:
+            user=None
+        if user is not None and generate_token.check_token(user,token):
+            user.is_active=True
+            user.save()
+            messages.info(request,"Account Activated Successfully")
+            return redirect('/auth/login/')
+        return render(request,'activatefail.html')
+
+
+
+
+def handleLogin(request):
+    if request.method=="POST":
+        username=request.POST['email']
+        userpassword=request.POST['pass1']
+        myuser=authenticate(username=username,password=userpassword)
+
+        if myuser is not None:
+            login(request,myuser)
+            messages.success(request,"Login Success")
+            return redirect('/')
+
+        else:
+            messages.error(request,"Invalid Credentials")
+            return redirect('/auth/login/')
+
+    return render(request, "login.html")
+
+
+
+
+def handleLogout(request):
+    logout(request)
+    messages.success(request,"Logout success")
+    return render(request,"login.html")
---
+++ Django-Startup-Project/Django-advance-project-STI-Bangalore/startupproject/startupapp/views.py
@@ -0,0 +1,127 @@
+from django.shortcuts import render,redirect
+from authapp.models import Contact
+from django.contrib import messages
+from startupapp.models import Courses,Register,Payments,Attendace
+# Create your views here.
+def index(request):
+    return render(request,"index.html")
+
+def about(request):
+    return render(request,"about.html")
+
+
+def contact(request):
+    if request.method=="POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phoneNo=request.POST.get('num')
+        desc=request.POST.get('desc')
+        query=Contact(name=name,email=email,phoneNumber=phoneNo,description=desc)
+        query.save()
+        messages.success(request,"Thanks for Contacting us we will get back you soon...")
+        return render(request,"contact.html")
+    return render(request,"contact.html")
+
+
+def courses(request):
+    courses=Courses.objects.all()
+    context={"courses":courses}
+    return render(request,"courses.html",context)
+
+def course(request,id):
+    course=Courses.objects.filter(courseName=id)
+    context={"course":course}
+    return render(request,"course.html",context)
+
+def enroll(request):
+    if not request.user.is_authenticated:
+        messages.warning(request,"Please Login & Register with us")
+        return redirect("/auth/login/")
+    courses=Courses.objects.all()
+    context={"courses":courses}
+# if the form is submitted this below logic is going to work 
+
+
+    if request.method=="POST":
+        fname=request.POST.get('fname')
+        lname=request.POST.get('lname')
+        fatherName=request.POST.get('fatherName')
+        phone=request.POST.get('phone')
+        alternateNumber=request.POST.get('alternateNumber')
+        email=request.POST.get('email')
+        college=request.POST.get('college')
+        addr=request.POST.get('addr')
+        landmark=request.POST.get('landmark')
+        street=request.POST.get('street')
+        pcode=request.POST.get('pcode')
+        city=request.POST.get('city')
+        companyname=request.POST.get('companyname')
+        Designation=request.POST.get('Designation')
+        Qualification=request.POST.get('Qualification')
+        cknowledge=request.POST.get('cknowledge')
+        scourse=request.POST.get('scourse')
+        ccourse=request.POST.get('ccourse')
+        emailPresent=Register.objects.filter(email=email)
+        if emailPresent:
+            messages.error(request,"Email is already Taken")
+            return redirect('/enroll/')
+
+        if scourse==ccourse:
+            pass
+        else:
+            messages.error(request,"Please Select the Valid Course...")
+            return redirect('/enroll/')
+        query=Register(firstName=fname,lastName=lname,fatherName=fatherName,phoneNumber=phone,alternateNumber=alternateNumber,email=email,collegeName=college,address=addr,landmark=landmark,street=street,city=city,pincode=pcode,companyName=companyname,designation=Designation,qualification=Qualification,computerKnowledge=cknowledge,Course=scourse)
+        # print(query.candidateId)
+        query.save()
+        messages.success(request,"Enrollment Success")
+        return redirect('/candidateprofile/')
+
+
+
+    return render(request,"enroll.html",context)
+
+
+
+# def candidateprofile(request):
+#     if not request.user.is_authenticated:
+#         messages.warning(request,"Please Login & View Your Profile")
+#         return redirect("/auth/login/")   
+#     currentuser=request.user.username
+#     # print(currentuser)
+#     details=Register.objects.filter(email=currentuser)
+#     payment=Payments.objects.all()
+#     paymentstatus=""
+#     amount=0
+#     balance=0
+#     for j in payment:
+#         if str(j.name)==currentuser :
+#             # print(j.name,type(str(j.name)))
+#             # print('matching')
+#             paymentstatus=j.status
+#             amount=j.amountPaid
+#             balance=j.balance
+
+#     # print(paymentstatus)
+#     # print(amount)
+#     # print(balance)
+#     # print(details)
+#     paymentstats={"paymentstatus":paymentstatus,"amount":amount,"balance":balance}
+
+#     attendanceStats=Attendace.objects.filter(email=currentuser)   
+#     context={"details":details,"status":paymentstats,"attendanceStats":attendanceStats}
+
+#     return render(request,"profile.html",context)
+
+
+
+
+# we will update candidate details here
+
+
+
+# will do apply attendance functionality
+
+
+
+# will do search functionality
